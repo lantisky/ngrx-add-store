f9f8f2ade675ef3615147abd1473c0a9f1752de7
lantisky <lantislexc@gmail.com>
2021-04-10 13:17:22 +1200

    init commit

---
+++ src/app/auth/store/auth.reducer.ts
@@ -0,0 +1,47 @@
+import { User } from '../user.model';
+import * as AuthActions from './auth.action';
+
+export interface State {
+  user: User;
+  authError: string;
+  loading: boolean;
+}
+
+const initalState = {
+  user: null,
+  authError: null,
+  loading: false,
+};
+
+export function authReducer(state, action: AuthActions.AuthActions) {
+  switch ( action.type ) {
+    case AuthActions.LOGIN:
+      const user = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.expirationDate);
+      return {
+        ...state,
+        authError: null,
+        user: user,
+        loading: false
+      };
+    case AuthActions.LOGIN_START:
+      return {
+        ...state,
+        authError: null,
+        loading: true,
+      };
+    case AuthActions.LOGIN_FAIL:
+      return {
+        ...state,
+        user: null,
+        authError: action.payload
+      }
+    case AuthActions.LOGOUT:
+    default:
+      return state;
+  }
+
+}
